41e2d150735b49220b12b4f895cd1e7ddccbee1d
Ran0357 <yuuma05290606@gmail.com>
2023-11-14 15:21:22 +0900

    kadaiAのメソッドかっこ忘れ修正

--- MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -3,13 +3,14 @@ package com.example.demo.controller;
 public class MergeHellController {
 	public static void main(String[] args) {
 		kadaiA();
-		kadaiB();
+	　  kadaiB();
 		kadaiC();
 	}
 
 	static void kadaiA(){
 	for(int i = 1; i < 11; i++) {
 		System.out.println("現在"+i+"回目のループです");
+	}
 	}
 	
 
