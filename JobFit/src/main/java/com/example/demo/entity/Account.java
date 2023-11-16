0f1ba55c000e09cba748a81c875eae2f426e5222
Ran0357 <yuuma05290606@gmail.com>
2023-11-16 10:56:35 +0900

    ControllerとEntityRepository追加 pom.xmlも編集

---
+++ JobFit/src/main/java/com/example/demo/entity/Account.java
@@ -0,0 +1,84 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+//↓「name="xxx"」の「xxx」の部分に模倣したいテーブル名を書く
+@Table(name = "Account")
+public class Account {
+
+	//主キーには「@Id」を設定する！
+	@Id
+	//カラム名(列名)を書く。
+	@Column(name = "UserID")
+	private int UserID;
+
+	@Column(name = "UserName")
+	private String UserName;
+
+	@Column(name = "Age")
+	private int Age;
+
+	@Column(name = "Gender")
+	private String Gender;
+	
+	@Column(name = "Password")
+	private int Password;
+	
+	@Column(name = "Mailaddress")
+	private String Mailaddress;
+
+	public int getUserID() {
+		return UserID;
+	}
+
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+
+	public int getAge() {
+		return Age;
+	}
+
+	public void setAge(int age) {
+		Age = age;
+	}
+
+	public String getGender() {
+		return Gender;
+	}
+
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+
+	public int getPassword() {
+		return Password;
+	}
+
+	public void setPassword(int password) {
+		Password = password;
+	}
+
+	public String getMailaddress() {
+		return Mailaddress;
+	}
+
+	public void setMailaddress(String mailaddress) {
+		Mailaddress = mailaddress;
+	}
+	
+	
+
+}
