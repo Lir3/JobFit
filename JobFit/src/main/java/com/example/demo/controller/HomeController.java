5eac4ffb978e12a231193ff1df7eee1e5c1449d3
S0629 <S0629@22S0629>
2023-12-04 16:22:03 +0900

    修正

--- JobFit/src/main/java/com/example/demo/controller/HomeController.java
+++ JobFit/src/main/java/com/example/demo/controller/HomeController.java
@@ -3,9 +3,10 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.entity.Account;
 import com.example.demo.repository.AccountRepository;
 
 import jakarta.servlet.http.HttpSession;
@@ -13,18 +14,19 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class HomeController {
 
-    @Autowired
-    private static AccountRepository accountRepository;
+	@Autowired
+    private AccountRepository accountRepository;
 
-    @GetMapping("/home")
+	@RequestMapping(path = "/home", method = RequestMethod.GET)
     public String home() {
         return "home";
     }
 
-    @GetMapping("/start")
+	@RequestMapping(path = "/start", method = RequestMethod.GET)
     public String start1(String username, String password, Model model, HttpSession session) {
         // ログインしている場合、診断の結果をセッションで保持
-        if (isUserLoggedIn(username, password)) {
+    	Account user = accountRepository.findByUsernameAndPassword(username, password);
+        if (user != null) {
             String diagnosisResult = (String) session.getAttribute("diagnosisResult");
             if (diagnosisResult != null) {
                 model.addAttribute("previousResult", diagnosisResult);
@@ -34,12 +36,9 @@ public class HomeController {
         return "start";
     }
 
-    private boolean isUserLoggedIn(String username, String password) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
-	}
+  
 
-	@PostMapping("/start")
+	@RequestMapping(path = "/start", method = RequestMethod.POST)
     public String start(Model model, HttpSession session) {
         // 診断の結果をセッションに保存
         String result = "Your diagnosis result"; // ここで実際の診断結果を取得
@@ -48,7 +47,7 @@ public class HomeController {
         return "redirect:/dashboard";
     }
 
-    @GetMapping("/dashboard")
+	@RequestMapping(path = "/dashboard", method = RequestMethod.GET)
     public String dashboard(Model model, HttpSession session) {
         // セッションから診断の結果を取得
         String diagnosisResult = (String) session.getAttribute("diagnosisResult");
