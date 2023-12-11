c5ac06df1ecaa3958acf2bffd7db09ed1c981fd7
S0631 <S0631@22S0631>
2023-12-11 12:17:19 +0900

    a

--- JobFit/src/main/java/com/example/demo/controller/LoginController.java
+++ JobFit/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,38 +8,58 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.entity.Account;
 import com.example.demo.repository.AccountRepository;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 	@Autowired
 	private AccountRepository accountRepository;
 	
+
+    @Autowired
+    private HttpSession httpSession; // HttpSessionをDI
+
 	@RequestMapping(path = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "login";
 	}
 	
+
 	@RequestMapping(path = "/loginpage", method = RequestMethod.GET)
     public String showLoginPage() {
         return "loginpage";
     }
 	
-	
-	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public String login(String username, String password, Model model) {
-		Account user = accountRepository.findByUsernameAndPassword(username, password);
-
-		if (user != null) {
-			// ログイン成功
-			return "redirect:/home";
-		} else {
-			// ログイン失敗
-			// ログイン失敗時の処理
-			model.addAttribute("error", "ユーザー名またはパスワードが正しくありません。");
-
-			return "redirect:/loginpage";
-
-		}
+	@RequestMapping(path = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "test";
 	}
+	
+	@RequestMapping(path = "/test2", method = RequestMethod.GET)
+    public String test2() {
+        return "test2t";
+	}
+	
+	@RequestMapping(path = "/loginpage", method = RequestMethod.POST)
+    public String login(String username, String password, Model model) {
+        Account user = accountRepository.findByUsernameAndPassword(username, password);
+        if (user != null) {
+            // ログイン成功
+            if ("admin".equals(username) && "admin".equals(password)) {
+                // 管理者ユーザーの場合は管理者画面に遷移
+                return "redirect:/test";
+            } else {
+                // 一般ユーザーの場合は診断画面に遷移
+                return "test2";
+            }
+        } else {
+            // ログイン失敗
+            // ログイン失敗時の処理
+            model.addAttribute("error", "ユーザー名またはパスワードが正しくありません。");
+            return "loginpage";
+        }
+    }
 	
 	
 	@RequestMapping(path = "/guest-login", method = RequestMethod.GET)
