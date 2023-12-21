df93573e96b025f796e12fcf5b7f7b0a9de16ce3
S0631 <S0631@22S0631>
2023-12-21 14:26:23 +0900

    session追加

--- JobFit/src/main/java/com/example/demo/controller/LoginController.java
+++ JobFit/src/main/java/com/example/demo/controller/LoginController.java
@@ -5,47 +5,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.example.demo.entity.Account;
 import com.example.demo.repository.AccountRepository;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
-	@Autowired
-	private AccountRepository accountRepository;
-	
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Autowired
     private HttpSession httpSession; // HttpSessionをDI
 
-	@RequestMapping(path = "/login", method = RequestMethod.GET)
-	public String login() {
-		return "login";
-	}
-	
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 
-	@RequestMapping(path = "/loginpage", method = RequestMethod.GET)
+    @RequestMapping(path = "/loginpage", method = RequestMethod.GET)
     public String showLoginPage() {
         return "loginpage";
     }
-	
-	@RequestMapping(path = "/test", method = RequestMethod.GET)
-    public String test() {
-        return "test";
-	}
-	
-	@RequestMapping(path = "/test2", method = RequestMethod.GET)
-    public String test2() {
-        return "test2t";
-	}
-	
-	@RequestMapping(path = "/loginpage", method = RequestMethod.POST)
-    public String login(String username, String password, Model model) {
+    
+
+    
+    @RequestMapping(path = "/loginpage", method = RequestMethod.POST)
+    public String login(String username, String password, Model model, SessionStatus sessionStatus) {
         Account user = accountRepository.findByUsernameAndPassword(username, password);
         if (user != null) {
             // ログイン成功
+            httpSession.setAttribute("loggedInUser", user); // ユーザーをセッションに保存
             if ("admin".equals(username) && "admin".equals(password)) {
                 // 管理者ユーザーの場合は管理者画面に遷移
                 return "redirect:/test";
@@ -57,24 +50,38 @@ public class LoginController {
             // ログイン失敗
             // ログイン失敗時の処理
             model.addAttribute("error", "ユーザー名またはパスワードが正しくありません。");
+            
             return "loginpage";
         }
     }
-	
-	
-	@RequestMapping(path = "/guest-login", method = RequestMethod.GET)
-	public String guestLogin(Model model) {
-		// ゲストユーザーのデータを指定してログイン
-		Account guestUser = accountRepository.findByUsername("guest");
-		if (guestUser != null) {
-			// ゲストユーザーとしてログイン成功
-			return "redirect:/home";
-		} else {
-			// ゲストユーザーが存在しない場合の処理
-			model.addAttribute("error", "ゲストユーザーが存在しません。");
-			return "redirect:/ng";
-		}
-		
-	}
-	
+
+    @RequestMapping(path = "/guest-login", method = RequestMethod.GET)
+    public String guestLogin(Model model) {
+        // ゲストユーザーのデータを指定してログイン
+        Account guestUser = accountRepository.findByUsername("guest");
+        if (guestUser != null) {
+            // ゲストユーザーとしてログイン成功
+            httpSession.setAttribute("loggedInUser", guestUser); // ゲストユーザーをセッションに保存
+            return "redirect:/home";
+        } else {
+            // ゲストユーザーが存在しない場合の処理
+            model.addAttribute("error", "ゲストユーザーが存在しません。");
+            return "redirect:/ng";
+        }
+    }
+    
+    @RequestMapping(path = "/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest request) {
+        // ログアウト時にセッションからユーザー情報を削除
+        httpSession.removeAttribute("loggedInUser");
+
+        // セッションを無効にする
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/login";
+    }
+
 }
